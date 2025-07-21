98e7f78075999c222bcf817fcc76e94c4740df1f
Jane Sinitsyn <sinitsyn.zena@yandex.ru>
2025-07-21 12:19:58 +0400

    Added /load_image function without db save

---
+++ core/bot.py
@@ -0,0 +1,55 @@
+import telebot
+from telebot import types
+import os
+from dotenv import load_dotenv
+
+load_dotenv("./.env")
+
+BOT_API_KEY = os.getenv("BOT_API_KEY")
+
+bot = telebot.TeleBot(BOT_API_KEY)
+
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(types.KeyboardButton("Загрузить изображение"))
+keyboard.add(types.KeyboardButton("Получить коллаж"))
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "Выберите действие:",
+        reply_markup=keyboard
+    )
+
+@bot.message_handler(func=lambda m: m.text == "Загрузить изображение")
+def request_image(message):
+    bot.send_message(
+        message.chat.id,
+        "Отправьте изображение для загрузки."
+    )
+    bot.register_next_step_handler(message, handle_uploaded_image)
+
+def handle_uploaded_image(message):
+    try:
+        if message.content_type != 'photo':
+            raise ValueError("Это не изображение!")
+
+        photo = message.photo[-1]
+        file_id = photo.file_id
+        file_info = bot.get_file(file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        os.makedirs("images", exist_ok=True)
+        file_path = f"images/{file_id}.jpg"
+        with open(file_path, 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+
+        bot.send_message(message.chat.id, "Изображение успешно загружено! (testing)")
+    except Exception as e:
+        bot.send_message(message.chat.id, "Ошибка: {e} (testing)")
+
+
+if __name__ == '__main__':
+    print("Бот запущен")
+    bot.infinity_polling()
