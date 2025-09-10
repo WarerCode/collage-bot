1aab7bca0f4aa8abebb914b66e0678a9fad11a0f
Danila <efimovdanila.de@gmail.col>
2025-09-10 14:47:53 +0400

    data pacckage done

--- core/warerobjects/data/userinfo.py
+++ core/warerobjects/data/userinfo.py
@@ -1,5 +1,8 @@
 """
+Класс для хранения данных о пользователе.
 
+Предоставляет интерфейс для хранения стандартных полей пользователя Telegram.
+Поля описаны во вложеном перечислителе, рекомендуется использовать.
 """
 
 import enum
@@ -7,16 +10,25 @@ import enum
 import core.warerobjects.warerobject as warer
 
 
-#TODO: пока что черновик
 class UserInfo(warer.WarerObject):
     """
+    Класс для хранения данных о пользователе Telegram.
 
+    Родители:
+        warer.WarerObject
+        abc.ABC
+
+    Аттрибуты:
+        user_id (int): уникальный внутренний идентификатор пользователя Telegram.
+        username (str):  имя пользователя.
     """
 
     class Fields(enum.Enum):
         """
-
+        Класс-перечислитель для унификации стандартных полей
+        пользователя Telegram.
         """
+
         FIRST_NAME = "first_name"
         LAST_NAME = "last_name"
         USERNAME = "username"
@@ -29,36 +41,31 @@ class UserInfo(warer.WarerObject):
         IS_PREMIUM = "is_premium"
         ABOUT = "about"
 
-
     def __init__(self,
                  user_id,
-                 username: str,
-                 age: int | None=None):
+                 username: str):
         """
         Инициализирует self.
         """
         super().__init__()
         self.user_id = user_id
         self.username = username
-        self.age = age
 
     def __repr__(self):
         """
         Возвращает отладочное представление данных.
         """
         return (f"MetaData(hints={self.hints}, "
-                f"user_id={self.user_id}, "
-                f"username={self.username}, "
-                f"age={self.age})")
+                f"id={self.user_id}, "
+                f"username={self.username})")
 
     def __str__(self):
         """
         Возвращает читаемое строковое представление данных.
         """
-        return (f"(hints={self.hints}, "
-                f"user_id={self.user_id}, "
-                f"username={self.username}, "
-                f"age={self.age})")
+        return (f"UserInfo(hints={self.hints}, "
+                f"id={self.user_id}, "
+                f"username={self.username})")
 
     def to_dict(self):
         """
@@ -66,9 +73,8 @@ class UserInfo(warer.WarerObject):
         """
         return {
             "hints": self.hints,
-            "user_id": self.user_id,
-            "username": self.username,
-            "age": self.age
+            "id": self.user_id,
+            "username": self.username
         }
 
     def reset_state(self):
@@ -87,7 +93,7 @@ if __name__ == "__main__":
     help(UserInfo)
 
     print("Пример использования класса UserInfo:")
-    userinfo = UserInfo(1234567890, "Данила", 20)
+    userinfo = UserInfo(1234567890, "Данила")
     print(userinfo)
     print()
 
