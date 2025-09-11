7cd5dede381e52bd3b5766084e7cf44a6de12116
Danila <efimovdanila.de@gmail.col>
2025-09-12 01:44:12 +0400

    using policy done

--- core/warerobjects/politics/using_limits.py
+++ core/warerobjects/politics/using_limits.py
@@ -1,43 +1,175 @@
 """
 Модуль содержит определения лимитов использования для разных типов пользователей.
 
-Содержит словари с ограничениями:
+Включает словари с ограничениями:
 - DEFAULT_LIMITS — лимиты для обычных пользователей.
 - SUBSCRIBER_LIMITS — лимиты для подписчиков.
 - BANNED_LIMITS — ограничения для заблокированных пользователей.
 - ADMIN_LIMITS — специальные права для администраторов.
 
+Класс UsingLimits предоставляет удобный интерфейс для доступа к этим лимитам по типу пользователя.
+
 TODO:
     - @JaneeWatermelon уточнить и согласовать реальные значения лимитов и эффектов.
 """
 
-NO_LIMIT = -1
+import enum
 
-DEFAULT_LIMITS = {
-    "collages_per_day": 100,
-    "effects": [
-        "",
-        "",
-        ""
-    ],
-    "loadings_per_day": 20
-}
+import core.warerobjects.warerobject as warer
 
-SUBSCRIBER_LIMITS = {
-    "collages_per_day": NO_LIMIT,
-    "effects": [
-        "",
-        "",
-        "",
-        ""
-    ],
-    "loadings_per_day": 100
-}
 
-BANNED_LIMITS = {
-    "ban": True
-}
+class UserType(enum.Enum):
+    """
+    Типы пользователей с соответствующими правами.
+    """
 
-ADMIN_LIMITS = {
-    "admin": True
-}
+    DEFAULT = "default"
+    SUBSCRIBER = "subscriber"
+    BOT = "bot"
+    BANNED = "banned"
+    ADMIN = "admin"
+
+class Permission(enum.Enum):
+    """
+    Перечисление доступных разрешений (прав) для пользователей.
+    """
+
+    # для обычных пользователей
+    UNLIMITED = "unlimited"
+    NO_PROMOTIONS = "no_promotions"
+    SEND_IMAGES = "send_images"
+    CREATE_COLLAGE = "create_collage"
+    EDIT_COLLAGE = "edit_collage"
+    MORE_EFFECTS = "more_effects"
+
+    # для супер пользователей
+    MANAGE_USERS = "manage_users"
+    ACCESS_STATS = "access_stats"
+    CREATE_POLL = "create_poll"
+
+
+NO_LIMIT = None
+
+
+# Атрибуты политики использования
+COLLAGES_PER_DAYS = "collages_per_day"
+EFFECTS = "effects"
+LOADINGS_PER_DAY = "loadings_per_day"
+NOTIFY_PERIOD = "notify_period"
+PERMISSIONS = "permissions"
+
+# Дефолтные значения эффектов и разрешений
+DEFAULT_EFFECTS = ["", "", ""]
+EXTENDED_EFFECTS = ["", "", "", ""]
+DEFAULT_PERMISSIONS = []
+SUBSCRIBE_PERMISSIONS = []
+ADMIN_PERMISSIONS = []
+BANNED_PERMISSIONS = []
+
+
+class UsingLimits(warer.WarerObject):
+    """
+    Класс, представляющий лимиты использования и разрешения для разных типов пользователей.
+
+    Родители:
+        warer.WarerObject
+        abc.ABC
+
+    Атрибуты класса:
+        DEFAULT_LIMITS: Лимиты для обычных пользователей.
+        SUBSCRIBER_LIMITS: Лимиты для подписчиков.
+        BANNED_LIMITS: Лимиты для заблокированных пользователей.
+        ADMIN_LIMITS: Лимиты для администраторов.
+        USING_POLITICS_LIMITS: Сопоставление типов пользователей с их лимитами.
+
+    Атрибут экземпляра:
+        value: Словарь лимитов для конкретного типа пользователя.
+    """
+
+    DEFAULT_LIMITS = {
+        COLLAGES_PER_DAYS: 100,
+        EFFECTS: DEFAULT_EFFECTS,
+        LOADINGS_PER_DAY: 20,
+        NOTIFY_PERIOD: 3,
+        PERMISSIONS: DEFAULT_PERMISSIONS
+    }
+
+    SUBSCRIBER_LIMITS = {
+        COLLAGES_PER_DAYS: NO_LIMIT,
+        EFFECTS: EXTENDED_EFFECTS,
+        LOADINGS_PER_DAY: 100,
+        NOTIFY_PERIOD: 7,
+        PERMISSIONS: SUBSCRIBE_PERMISSIONS
+    }
+
+    BANNED_LIMITS = {
+        COLLAGES_PER_DAYS: 0,
+        EFFECTS: [],
+        LOADINGS_PER_DAY: 0,
+        NOTIFY_PERIOD: 2,
+        PERMISSIONS: BANNED_PERMISSIONS
+    }
+
+    ADMIN_LIMITS = {
+        COLLAGES_PER_DAYS: NO_LIMIT,
+        EFFECTS: EXTENDED_EFFECTS,
+        LOADINGS_PER_DAY: NO_LIMIT,
+        NOTIFY_PERIOD: NO_LIMIT,
+        PERMISSIONS: ADMIN_PERMISSIONS
+    }
+
+    USING_POLITICS_LIMITS = {
+        UserType.DEFAULT: DEFAULT_LIMITS,
+        UserType.SUBSCRIBER: SUBSCRIBER_LIMITS,
+        UserType.BOT: DEFAULT_LIMITS,
+        UserType.BANNED: BANNED_LIMITS,
+        UserType.ADMIN: ADMIN_LIMITS
+    }
+
+    def __init__(self, user_type: UserType):
+        """
+        Инициализирует объект лимитов для указанного типа пользователя.
+
+        Параметры:
+            user_type (UserType): Тип пользователя.
+        """
+        super().__init__()
+        self.value = UsingLimits.USING_POLITICS_LIMITS[user_type]
+
+    def __str__(self):
+        """
+        Возвращает строковое представление объекта для удобного вывода.
+        """
+        return f"UsingLimits(value={self.value})"
+
+    def __repr__(self):
+        """
+        Возвращает формальное строковое представление объекта.
+        """
+        return f"UsingLimits(value={self.value})"
+
+    def to_dict(self):
+        """
+        Возвращает содержимое объекта в виде словаря.
+        """
+        return {
+            "value": self.value
+        }
+
+
+
+if __name__ == "__main__":
+    print("пример использования политик:")
+    help(UsingLimits)
+    print()
+
+    for user_type in UserType:
+        limits = UsingLimits(user_type)
+
+        print(f"Лимиты для пользователя типа '{user_type.value}':")
+        print(f"  Коллажей в день: {limits.value[COLLAGES_PER_DAYS]}")
+        print(f"  Эффекты: {limits.value[EFFECTS]}")
+        print(f"  Загрузка в день: {limits.value[LOADINGS_PER_DAY]}")
+        print(f"  Период уведомления (дней): {limits.value[NOTIFY_PERIOD]}")
+        print(f"  Разрешения: {limits.value[PERMISSIONS]}")
+        print()
--- core/warerobjects/politics/using_policy.py
+++ core/warerobjects/politics/using_policy.py
@@ -7,29 +7,8 @@
 - UsingPolicy.UserType — перечисление типов пользователей с соответствующими наборами прав.
 """
 
-import enum
-
 import core.warerobjects.politics.basepolicy as policy
 import core.warerobjects.politics.using_limits as using_limits
-
-
-class Permission(enum.Enum):
-    """
-    Перечисление доступных разрешений (прав) для пользователей.
-    """
-
-    # для смертных пользователей
-    UNLIMITED = "unlimited"
-    NO_PROMOTIONS = "no_promotions"
-    SEND_IMAGES = "send_images"
-    CREATE_COLLAGE = "create_collage"
-    EDIT_COLLAGE = "edit_collage"
-    MORE_EFFECTS = "more_effects"
-
-    # для супер пользователей
-    MANAGE_USERS = "manage_users"
-    ACCESS_STATS = "access_stats"
-    CREATE_POLL = "create_poll"
 
 
 class UsingPolicy(policy.Policy):
@@ -47,22 +26,7 @@ class UsingPolicy(policy.Policy):
         _limits (dict): словарь с лимитами для данного типа пользователя.
     """
 
-    class UserType(enum.Enum):
-        """
-        Типы пользователей с соответствующими правами.
-        """
-
-        DEFAULT = [
-            Permission.SEND_IMAGES.value,
-            Permission.CREATE_COLLAGE.value,
-            Permission.EDIT_COLLAGE.value,
-        ]
-        SUBSCRIBER = [p for p in DEFAULT] + [
-            Permission.UNLIMITED.value,
-            Permission.NO_PROMOTIONS.value
-        ]
-        BANNED = []
-        ADMIN = [p.value for p in Permission]
+    UserType = using_limits.UserType
 
     def __init__(self, usertype: UserType):
         """
@@ -72,36 +36,34 @@ class UsingPolicy(policy.Policy):
             usertype (UserType): тип пользователя, определяющий права и лимиты.
         """
         super().__init__()
-        self._usertype = usertype
-        self._permissions = usertype.value
-
-        self._limits = {}
-        self.__set_limits()
+        self._usertype = usertype.value
+        self._limits = using_limits.UsingLimits(user_type)
+        self._notify_policy = None
 
     def __str__(self):
         """
         Строковое представление объекта.
         """
-        return (f"UsingPolicy(usertype={self._usertype}, "
-                f"permissions={self._permissions}, "
-                f"limits={self._limits})")
+        return (f"UsingPolicy(usertype={self._usertype},"
+                f"limits={self._limits},"
+                f"notify_policy={self._notify_policy})")
 
     def __repr__(self):
         """
         Формальное строковое представление объекта.
         """
-        return (f"(usertype={self._usertype}, "
-                f"permissions={self._permissions}, "
-                f"limits={self._limits})")
+        return (f"(usertype={self._usertype},"
+                f"limits={self._limits},"
+                f"notify_policy={self._notify_policy})")
 
     def to_dict(self):
         """
         Представление объекта в виде словаря.
         """
         return {
-            "usertype": self._usertype.value,
-            "permissions": self._permissions,
-            "limits": self._limits
+            "usertype": self._usertype,
+            "limits": self._limits,
+            "notify_policy": self._notify_policy
         }
 
     def get_limits(self):
@@ -110,44 +72,27 @@ class UsingPolicy(policy.Policy):
         """
         return self._limits
 
-    def __set_limits(self):
-        """
-        Устанавливает лимиты в зависимости от типа пользователя.
-        """
-        if self._usertype == UsingPolicy.UserType.DEFAULT:
-            self._limits = using_limits.DEFAULT_LIMITS
-
-        elif self._usertype == UsingPolicy.UserType.BANNED:
-            self._limits = using_limits.BANNED_LIMITS
-
-        elif self._usertype == UsingPolicy.UserType.SUBSCRIBER:
-            self._limits = using_limits.SUBSCRIBER_LIMITS
-
-        elif self._usertype == UsingPolicy.UserType.ADMIN:
-            self._limits = using_limits.ADMIN_LIMITS
-
-        else:
-            # TODO: сделать свои классы ошибок и здесь выбросить исключение
-            raise ValueError("Неизвестный тип пользователя")
 
 
 if __name__ == "__main__":
     print("Класс политики использования с разграничением прав и лимитов:")
     help(UsingPolicy)
 
+    UserType = UsingPolicy.UserType
+
     print("__str__:")
-    for user_type in UsingPolicy.UserType:
+    for user_type in UserType:
         using = UsingPolicy(user_type)
         print(f"Политика использования для {user_type.name}: {using}")
     print()
 
     print("__repr__:")
-    for user_type in UsingPolicy.UserType:
+    for user_type in UserType:
         using = UsingPolicy(user_type)
         print(f"Политика использования для {user_type.name}: {using.__repr__()}")
     print()
 
     print("to_dict:")
-    for user_type in UsingPolicy.UserType:
+    for user_type in UserType:
         using = UsingPolicy(user_type)
         print(f"Политика использования для {user_type.name}: {using.to_dict()}")
